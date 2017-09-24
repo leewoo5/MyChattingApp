9d55537b8192def5ae8ebbc7bca7c62fd9489f40
unknown <leewoo5629@gmail.com>
2017-09-24 19:33:39 +0900

    login page add

--- app/src/main/java/com/topfactory/leewoo5629/mychattingapp/AuthActivity.java
+++ app/src/main/java/com/topfactory/leewoo5629/mychattingapp/AuthActivity.java
@@ -1,15 +1,19 @@
 package com.topfactory.leewoo5629.mychattingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class AuthActivity extends AppCompatActivity {
+public class AuthActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText mIdEditText;
     private EditText mPasswordEditText;
     private Button mConfirm;
+    private String mPageType;
+    private Boolean mIsSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,30 @@ public class AuthActivity extends AppCompatActivity {
         mIdEditText = (EditText) findViewById(R.id.IdEditText);
         mPasswordEditText = (EditText) findViewById(R.id.PasswordEditText);
         mConfirm = (Button) findViewById(R.id.button_confirm);
+
+        Intent intent = getIntent();
+        Boolean isSignUp = intent.getBooleanExtra("isSignUp", false);
+//        if (isSignUp) {
+//            mPageType = "Sign Up";
+//        } else {
+//            mPageType = "LogIn";
+//        }
+        mPageType = isSignUp ? "Sign Up" : "Login";
+        mIsSignUp = isSignUp;
+        mConfirm.setText(mPageType);
+        mConfirm.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(mIdEditText.getText().toString() != ""
+                && mPasswordEditText.getText().toString() != "") {
+            if(mIsSignUp) {
+
+            } else {
+
+            }
+        }
     }
 }
--- app/src/main/java/com/topfactory/leewoo5629/mychattingapp/LaunchActivity.java
+++ app/src/main/java/com/topfactory/leewoo5629/mychattingapp/LaunchActivity.java
@@ -1,5 +1,6 @@
 package com.topfactory.leewoo5629.mychattingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,16 +19,25 @@ public class LaunchActivity extends AppCompatActivity implements View.OnClickLis
         mButtonSignUp = (Button) findViewById(R.id.button_SignUp);
         mButtonLogin = (Button) findViewById(R.id.button_Login);
 
+        mButtonSignUp.setOnClickListener(this);
+        mButtonLogin.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
+        Intent intent = new Intent(LaunchActivity.this, AuthActivity.class);
         switch (view.getId()){
             case (R.id.button_SignUp):
-
+                intent.putExtra("isSignUp", true);
                 break;
             case (R.id.button_Login):
+
+                break;
+            default:
+                intent.putExtra("isSignUp", false);
                 break;
         }
+        startActivity(intent);
     }
 }
